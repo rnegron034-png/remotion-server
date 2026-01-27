95c21c28068d5b713b8ac89f2b1a42d13098c7e4
rnegron034-png <rnegron034@gmail.com>
2026-01-28 04:12:15 +0600

    Update index.tsx

--- remotion/index.tsx
+++ remotion/index.tsx
@@ -1,21 +1,4 @@
-import { registerRoot, Composition } from 'remotion';
-import { CreateVideo } from './Video';
+import { registerRoot } from "remotion";
+import { Video } from "./Video";
 
-registerRoot(() => {
-  return (
-    <>
-      <Composition
-        id="Video"  // ← Change this from "CreateVideo" to "Video"
-        component={CreateVideo}
-        durationInFrames={300}
-        fps={30}
-        width={1920}
-        height={1080}
-        defaultProps={{
-          scenes: [],
-          audio: ''
-        }}
-      />
-    </>
-  );
-});
+registerRoot(Video);
