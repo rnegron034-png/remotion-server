7cbb3d3bd8c13bb6b56a9284098de1c577371a1e
rnegron034-png <rnegron034@gmail.com>
2026-01-28 05:20:24 +0600

    Update VideoComposition.js

--- remotion/src/VideoComposition.js
+++ remotion/src/VideoComposition.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Composition, staticFile } from 'remotion';
+import { Composition } from 'remotion';
 import { VideoSequence } from './VideoSequence.js';
 
 export const VideoComposition = () => {
@@ -11,16 +11,6 @@ export const VideoComposition = () => {
       fps={30}
       width={1920}
       height={1080}
-      calculateMetadata={async ({ props }) => {
-        // Props from CLI are now available here
-        const scenes = props?.scenes || [];
-        const totalDuration = scenes.length * 150; // 150 frames per scene
-        
-        return {
-          durationInFrames: totalDuration > 0 ? totalDuration : 300,
-          props: props || { scenes: [], audio: null }
-        };
-      }}
     />
   );
 };
