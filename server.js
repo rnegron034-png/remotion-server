4f11e59ab5cdc4d76439d9f7e9a6b90a69e50d7a
rnegron034-png <rnegron034@gmail.com>
2026-01-27 23:41:05 +0600

    Update server.js

--- server.js
+++ server.js
@@ -96,14 +96,20 @@ app.post("/remotion-render", async (req, res) => {
         /* 4) Render */
         const video = path.join(dir, "video.mp4");
 
-        const cmd = `npx remotion render remotion/index.ts Video "${video}"
-  --props="${props}"
-  --codec=h264
-  --browser-executable=/usr/bin/chromium
-  --chromium-flags="--no-sandbox --disable-setuid-sandbox --disable-dev-shm-usage --disable-gpu --single-process --no-zygote"
-  --log=verbose`.replace(/\n/g, " ");
+       const cmd = [
+  "npx remotion render",
+  "remotion/index.ts",
+  "Video",
+  `"${video}"`,
+  `--props="${props}"`,
+  "--codec=h264",
+  "--browser-executable=/usr/bin/chromium",
+  '--chromium-flags="--no-sandbox --disable-setuid-sandbox --disable-dev-shm-usage --disable-gpu --single-process --no-zygote"',
+  "--log=verbose",
+].join(" ");
 
 await run(cmd);
+
 
 
         await run(cmd);
