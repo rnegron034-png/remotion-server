f3c3d5f62c6183cdfee60d8304722ffbe77c86c7
rnegron034-png <rnegron034@gmail.com>
2026-01-28 05:04:18 +0600

    Update VideoComposition.js

--- remotion/src/VideoComposition.js
+++ remotion/src/VideoComposition.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Composition } from 'remotion';
-import { VideoSequence } from './VideoSequence';
+import { VideoSequence } from './VideoSequence.js';
 
 export const VideoComposition = () => {
   return (
     <Composition
       id="VideoComposition"
       component={VideoSequence}
-      durationInFrames={300} // 10 seconds at 30fps (will be overridden by actual video length)
+      durationInFrames={300}
       fps={30}
       width={1920}
       height={1080}
