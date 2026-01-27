e4f11221f6b7c68003d93775531cf9e0aad7e30a
rnegron034-png <rnegron034@gmail.com>
2026-01-28 04:12:38 +0600

    Update Video.tsx

--- remotion/Video.tsx
+++ remotion/Video.tsx
@@ -1,30 +1,13 @@
-import {AbsoluteFill, Audio, Img, Sequence, Video as RemotionVideo} from 'remotion';
+import { AbsoluteFill, Video as RVideo, Audio } from "remotion";
 
-export const Video = ({scenes, audio}) => {
+export const Video = ({ scenes, audio }) => {
   return (
-    <AbsoluteFill style={{backgroundColor: 'black'}}>
-      {scenes.map((scene, i) => {
-        const fromFrame = Math.round(scene.start * scene.fps || 30);
-        const durationFrames = Math.round(scene.duration * scene.fps || 30);
+    <AbsoluteFill>
+      {scenes.map((s, i) => (
+        <RVideo key={i} src={s.src} />
+      ))}
 
-        return (
-          <Sequence key={i} from={fromFrame} durationInFrames={durationFrames}>
-            {scene.type === "video" ? (
-              <RemotionVideo src={scene.local} />
-            ) : (
-              <Img src={scene.local} style={{objectFit: 'cover'}} />
-            )}
-          </Sequence>
-        );
-      })}
-
-      {audio && (
-        <Audio
-          src={audio.local}
-          startFrom={audio.trim ? Math.round(audio.trim[0] * 30) : 0}
-          durationInFrames={audio.trim ? Math.round((audio.trim[1] - audio.trim[0]) * 30) : undefined}
-        />
-      )}
+      {audio && <Audio src={audio.src} />}
     </AbsoluteFill>
   );
 };
