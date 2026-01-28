192217f8be77f76c324ecd3bfdb326d9b43b18b5
rnegron034-png <rnegron034@gmail.com>
2026-01-28 06:51:42 +0600

    Update VideoComposition.js

--- src/VideoComposition.js
+++ src/VideoComposition.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Composition } from 'remotion';
 import { VideoSequence } from './VideoSequence.js';
 
-export const VideoComposition = () => {
-  return (
-    <Composition
-      id="VideoComposition"
-      component={VideoSequence}
-      durationInFrames={300}
-      fps={30}
-      width={1080}
-      height={1920}
-    />
-  );
-};
+export const VideoComposition = () => (
+  <Composition
+    id="VideoComposition"
+    component={VideoSequence}
+    width={1080}
+    height={1920}
+    fps={30}
+    durationInFrames={150}
+  />
+);
